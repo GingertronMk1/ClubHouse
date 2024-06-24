6b564cc5f163e4646c2496587ee1bf01e110af76
Jack Ellis <jackellis1504@gmail.com>
2024-06-24 17:59:37 +0100

    register route
     M composer.json
     M composer.lock
     M src/Framework/Controller/SecurityController.php
     M symfony.lock
     M templates/base.html.twig
    ?? config/packages/validator.yaml
    ?? src/Application/User/
    ?? src/Framework/Form/
    ?? templates/security/register.html.twig

---
+++ src/Application/User/Command/CreateUserCommand.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Application\User\Command;
+
+class CreateUserCommand {
+    public function __construct(
+        public string $email = '',
+        public string $password = '',
+    ) {}
+}
