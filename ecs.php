6574ac5e97703f9c5a7a0280195361e0fede5cef
Jack Ellis <jackellis1504@gmail.com>
2024-06-24 13:19:09 +0100

    wip
     M composer.json
     M ecs.php

--- ecs.php
+++ ecs.php
@@ -2,20 +2,19 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 return ECSConfig::configure()
     ->withPaths([
-        __DIR__ . '/config',
-        __DIR__ . '/migrations',
-        __DIR__ . '/public',
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
+        __FILE__,
+        __DIR__.'/config',
+        __DIR__.'/migrations',
+        __DIR__.'/public',
+        __DIR__.'/src',
+        __DIR__.'/tests',
     ])
     // add sets - group of rules
-   ->withPhpCsFixerSets(
+    ->withPhpCsFixerSets(
         phpCsFixer: true
     )
-     
-    ;
+;
