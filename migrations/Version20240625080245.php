850b0b47ba6b02d1c14386d6b7c4d37aebe2da29
Jack Ellis <jackellis1504@gmail.com>
2024-06-25 09:30:52 +0100

    wip
     M migrations/Version20240625080245.php

--- migrations/Version20240625080245.php
+++ migrations/Version20240625080245.php
@@ -20,7 +20,40 @@ final class Version20240625080245 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $table = $schema->createTable('people');
+        $table->addColumn(
+            'id',
+            'string',
+        );
+        $table->addColumn(
+            'name',
+            'string',
+        );
+        $table->addColumn(
+            'user_id',
+            'string',
+            [
+                'notNull' => false
+            ]
+        );
+        $table->addColumn(
+            'created_at',
+            'string',
+        );
+        $table->addColumn(
+            'updated_at',
+            'string',
+        );
+        $table->addColumn(
+            'deleted_at',
+            'string',
+            [
+                'notNull' => false
+            ]
+        );
 
+        $table->setPrimaryKey(['id']);
+        $table->addForeignKeyConstraint('users', ['user_id'], ['id']);
     }
 
     public function down(Schema $schema): void
