e41e39002bc3a8a5f6ac139e81897e6979bfb549
Jack Ellis <jackellis1504@gmail.com>
2024-06-29 19:17:09 +0100

    ux improvements
     M src/Domain/MatchPerson/MatchPersonEntity.php
     D src/Domain/MatchPerson/ValueObject/MatchPersonId.php
     M src/Framework/Controller/MatchPersonController.php
     M templates/_util/make-entity/id.php.twig
     M templates/match/index.html.twig
    ?? migrations/Version20240629170420.php
    ?? templates/match-person/

--- src/Framework/Controller/MatchPersonController.php
+++ src/Framework/Controller/MatchPersonController.php
@@ -4,20 +4,27 @@ declare(strict_types=1);
 
 namespace App\Framework\Controller;
 
+use App\Application\Match\MatchFinderInterface;
 use App\Application\MatchPerson\MatchPersonFinderInterface;
+use App\Domain\Match\ValueObject\MatchId;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route(path: '/match/{matchId}', name: 'match.player.')]
+#[Route(path: '/match/{matchId}/person', name: 'match.person.')]
 class MatchPersonController extends AbstractController
 {
     #[Route(path: '/', name: 'index')]
     public function index(
         string $matchId,
+        MatchFinderInterface $matchFinder,
         MatchPersonFinderInterface $matchPersonFinder
     ): Response
     {
-        
+        $matchId = MatchId::fromString($matchId);
+        return $this->render('match-person/index.html.twig', [
+            'match' => $matchFinder->getById($matchId),
+            'people' => $matchPersonFinder->getForMatch($matchId)
+        ]);
     }
 }
